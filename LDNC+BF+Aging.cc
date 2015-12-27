18dd3722e29dd28cd566eb68570c4625d658f0ba
Kavé Salamatian <kave.salamatian@univ-savoie.fr>
2015-12-27 15:03:07 +0100

    still first bug

--- LDNC+BF+Aging.cc
+++ LDNC+BF+Aging.cc
@@ -1446,7 +1446,7 @@ void MyNCApp::PacketInjector ()
    else {
 		Ptr<NetworkCodedDatagram> p;
 		p=m_buffer.back();
-		Ptr<DecodedPacketStorage> q = CreateOject<DecodedPacketStorage>();
+		Ptr<DecodedPacketStorage> q = CreateObject<DecodedPacketStorage>();
 		q->ncDatagram=p;
     Ptr<NCAttribute> attribute= CreateObject<NCAttribute>();
     q->attribute.m_nodeId=p->m_coefsList.begin()->second.GetNodeId();
